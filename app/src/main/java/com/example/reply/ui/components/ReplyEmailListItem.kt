b77d0488372fb051060c33a645b0f0a9112568a3
Kacper Misterski <36102665+KCPR13@users.noreply.github.com>
2023-06-21 17:27:13 +0200

    implementation of typography

--- app/src/main/java/com/example/reply/ui/components/ReplyEmailListItem.kt
+++ app/src/main/java/com/example/reply/ui/components/ReplyEmailListItem.kt
@@ -79,9 +79,13 @@ fun ReplyEmailListItem(
                 ) {
                     Text(
                         text = email.sender.firstName,
+                        style = MaterialTheme.typography.labelMedium
+
                     )
                     Text(
                         text = email.createdAt,
+                        style = MaterialTheme.typography.labelMedium
+
                     )
                 }
                 IconButton(
@@ -99,10 +103,12 @@ fun ReplyEmailListItem(
 
             Text(
                 text = email.subject,
+                style = MaterialTheme.typography.titleLarge,
                 modifier = Modifier.padding(top = 12.dp, bottom = 8.dp),
             )
             Text(
                 text = email.body,
+                style = MaterialTheme.typography.bodyLarge,
                 maxLines = 2,
                 overflow = TextOverflow.Ellipsis
             )
